d22dda20ced1dd704b196b043ad7426bcbe2b352
ahmerhabib <60505302+ahmerhh@users.noreply.github.com>
2023-07-31 22:45:07 +0100

    Update test+src index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,163 @@
+import Shader from '@ahmerhh/WebGraphicLibrary-shader';
+
 /**
  * @class Program
  */
-export default class Program {}
+export default class Program {
+  /**
+   * @constructs Program
+   * @param {WebGLRenderingContext} gl
+   * @param {Shader|string} vertex - vertex shader
+   * @param {Shader|string} fragment - fragment shader
+   */
+  constructor(gl, vertex, fragment) {
+    this.gl = gl;
+
+    // If the vertex parameter is a string, create a new Shader instance with the vertex shader source
+    if (typeof vertex === 'string') {
+      vertex = new Shader(this.gl, this.gl.VERTEX_SHADER, vertex);
+    }
+
+    // If the fragment parameter is a string, create a new Shader instance with the fragment shader source
+    if (typeof fragment === 'string') {
+      fragment = new Shader(this.gl, this.gl.FRAGMENT_SHADER, fragment);
+    }
+
+    this.vertexShader = vertex;
+    this.fragmentShader = fragment;
+
+    // Create the WebGL program
+    this.program = this.gl.createProgram();
+
+    // Attach the vertex and fragment shaders to the program
+    this.gl.attachShader(this.program, this.vertexShader.shader);
+    this.gl.attachShader(this.program, this.fragmentShader.shader);
+
+    // Link the program
+    this.gl.linkProgram(this.program);
+
+    // Check if program linking was successful
+    if (!this.gl.getProgramParameter(this.program, this.gl.LINK_STATUS)) {
+      throw new Error('Error while linking program');
+    }
+
+    // Create empty objects to store attributes and uniforms information
+    this._attributes = {};
+    this._uniforms = {};
+  }
+
+  /**
+   * @method addAttribute
+   * @public
+   * @param {string} name
+   * @param {uint} size
+   * @param {uint} type
+   */
+  addAttribute(name, size, type) {
+    const location = this.gl.getAttribLocation(this.program, name);
+
+    this._attributes[name] = {
+      location,
+      type,
+      size,
+    };
+
+    // Enable the attribute location
+    this.gl.enableVertexAttribArray(location);
+  }
+
+  /**
+   * @method setAttributePointer
+   * @public
+   * @param {string} name
+   */
+  setAttributePointer(name) {
+    const { location, size, type } = this._attributes[name];
+
+    // Set the vertex attribute pointer
+    this.gl.vertexAttribPointer(location, size, type, false, 0, 0);
+  }
+
+  /**
+   * @method addUniform
+   * @public
+   * @param {string} name
+   * @param {uint} type
+   */
+  addUniform(name, type) {
+    const location = this.gl.getUniformLocation(this.program, name);
+
+    this._uniforms[name] = {
+      location,
+      type,
+    };
+  }
+
+  /**
+   * @method setUniform
+   * @public
+   * @param {string} name
+   * @param {any} value
+   */
+  setUniform(name, value) {
+    const { location, type } = this._uniforms[name];
+
+    // Set the uniform value based on its type
+    switch (type) {
+      case this.gl.INT:
+        this.gl.uniform1i(location, value);
+        break;
+      case this.gl.INT_VEC2:
+        this.gl.uniform2iv(location, value);
+        break;
+      case this.gl.INT_VEC3:
+        this.gl.uniform3iv(location, value);
+        break;
+      case this.gl.INT_VEC4:
+        this.gl.uniform4iv(location, value);
+        break;
+      case this.gl.FLOAT:
+        this.gl.uniform1f(location, value);
+        break;
+      case this.gl.FLOAT_VEC2:
+        this.gl.uniform2fv(location, value);
+        break;
+      case this.gl.FLOAT_VEC3:
+        this.gl.uniform3fv(location, value);
+        break;
+      case this.gl.FLOAT_VEC4:
+        this.gl.uniform4fv(location, value);
+        break;
+      case this.gl.FLOAT_MAT2:
+        this.gl.uniformMatrix2fv(location, false, value);
+        break;
+      case this.gl.FLOAT_MAT3:
+        this.gl.uniformMatrix3fv(location, false, value);
+        break;
+      case this.gl.FLOAT_MAT4:
+        this.gl.uniformMatrix4fv(location, false, value);
+        break;
+    }
+  }
+
+  /**
+   * @method bind
+   * @public
+   */
+  bind() {
+    this.gl.useProgram(this.program);
+  }
+
+  /**
+   * @method dispose
+   * @public
+   */
+  dispose() {
+    this.gl.deleteProgram(this.program);
+    this.program = null;
+
+    // Dispose the vertex and fragment shaders
+    this.vertexShader.dispose();
+    this.fragmentShader.dispose();
+  }
+}
--- test/index.js
+++ test/index.js
@@ -28,48 +28,60 @@ Test('Program class', t => {
   t.test('should be instanciable', t => {
     t.plan(1);
 
+    // Create a new Program instance with the vertex and fragment shader sources
     const program = new Program(gl, vertexShaderSource, fragmentShaderSource);
 
+    // Assert that the instance is of the correct class
     t.ok(program instanceof Program, 'instance of Program');
   });
 
   t.test('should accept attributes', t => {
     t.plan(1);
 
+    // Create a new Program instance with the vertex and fragment shader sources
     const program = new Program(gl, vertexShaderSource, fragmentShaderSource);
     program.addAttribute('aPosition', 3, gl.FLOAT);
 
+    // Get the number of active attributes in the program
     const attributesCount = gl.getProgramParameter(program.program, gl.ACTIVE_ATTRIBUTES);
 
+    // Assert that there is one active attribute in the program
     t.equal(attributesCount, 1, '1 active attribute');
   });
 
   t.test('should accept uniforms', t => {
     t.plan(1);
 
+    // Create a new Program instance with the vertex and fragment shader sources
     const program = new Program(gl, vertexShaderSource, fragmentShaderSource);
     program.addUniform('uColor', gl.FLOAT_VEC4);
 
+    // Get the number of active uniforms in the program
     const uniformsCount = gl.getProgramParameter(program.program, gl.ACTIVE_UNIFORMS);
 
+    // Assert that there is one active uniform in the program
     t.equal(uniformsCount, 1, '1 active uniform');
   });
 
   t.test('should set active program when binded', t => {
     t.plan(1);
 
+    // Create a new Program instance with the vertex and fragment shader sources
     const program = new Program(gl, vertexShaderSource, fragmentShaderSource);
     program.bind();
 
+    // Assert that the program is the active program in the WebGL context
     t.equal(gl.getParameter(gl.CURRENT_PROGRAM), program.program, 'program is active');
   });
 
   t.test('should be disposable', t => {
     t.plan(1);
 
+    // Create a new Program instance with the vertex and fragment shader sources
     const program = new Program(gl, vertexShaderSource, fragmentShaderSource);
     program.dispose();
 
+    // Assert that the program is deleted
     t.equal(program.program, null, 'program deleted');
   });
 
@@ -78,81 +90,67 @@ Test('Program class', t => {
 
 // Program.js
 class Program {
-    constructor(gl, vertexShaderSource, fragmentShaderSource) {
-      this.gl = gl;
-      this.program = null;
-  
-      this.init(vertexShaderSource, fragmentShaderSource);
+  constructor(gl, vertexShaderSource, fragmentShaderSource) {
+    this.gl = gl;
+    this.program = null;
+
+    // Initialize the program by creating and linking the shaders
+    this.init(vertexShaderSource, fragmentShaderSource);
+  }
+
+  init(vertexShaderSource, fragmentShaderSource) {
+    const gl = this.gl;
+    const vertexShader = this.createShader(gl.VERTEX_SHADER, vertexShaderSource);
+    const fragmentShader = this.createShader(gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+    this.program = this.createProgram(vertexShader, fragmentShader);
+  }
+
+  createShader(type, source) {
+    const gl = this.gl;
+    const shader = gl.createShader(type);
+
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      // Throw an error if shader compilation fails
+      throw new Error('An error occurred compiling the shader: ' + gl.getShaderInfoLog(shader));
     }
-  
-    init(vertexShaderSource, fragmentShaderSource) {
-      const gl = this.gl;
-      const vertexShader = this.createShader(gl.VERTEX_SHADER, vertexShaderSource);
-      const fragmentShader = this.createShader(gl.FRAGMENT_SHADER, fragmentShaderSource);
-  
-      this.program = this.createProgram(vertexShader, fragmentShader);
+
+    return shader;
+  }
+
+  createProgram(vertexShader, fragmentShader) {
+    const gl = this.gl;
+    const program = gl.createProgram();
+
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+      // Throw an error if program linking fails
+      throw new Error('Unable to initialize the shader program: ' + gl.getProgramInfoLog(program));
     }
-  
-    createShader(type, source) {
-      const gl = this.gl;
-      const shader = gl.createShader(type);
-  
-      gl.shaderSource(shader, source);
-      gl.compileShader(shader);
-  
-      if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        throw new Error('An error occurred compiling the shader: ' + gl.getShaderInfoLog(shader));
-      }
-  
-      return shader;
-    }
-  
-    createProgram(vertexShader, fragmentShader) {
-      const gl = this.gl;
-      const program = gl.createProgram();
-  
-      gl.attachShader(program, vertexShader);
-      gl.attachShader(program, fragmentShader);
-      gl.linkProgram(program);
-  
-      if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-        throw new Error('Unable to initialize the shader program: ' + gl.getProgramInfoLog(program));
-      }
-  
-      return program;
-    }
-  
-    bind() {
-      const gl = this.gl;
-      gl.useProgram(this.program);
-    }
-  
-    addAttribute(name, size, type) {
-      const gl = this.gl;
-      const attributeLocation = gl.getAttribLocation(this.program, name);
-  
-      if (attributeLocation >= 0) {
-        gl.vertexAttribPointer(attributeLocation, size, type, false, 0, 0);
-        gl.enableVertexAttribArray(attributeLocation);
-      }
-    }
-  
-    addUniform(name, type) {
-      const gl = this.gl;
-      const uniformLocation = gl.getUniformLocation(this.program, name);
-  
-      if (uniformLocation !== null) {
-        // Example: gl.uniform4f(uniformLocation, 1.0, 0.0, 0.0, 1.0);
-        // You can set uniform values as needed based on the type.
-      }
-    }
-  
-    dispose() {
-      const gl = this.gl;
-      gl.deleteProgram(this.program);
-      this.program = null;
+
+    return program;
+  }
+
+  bind() {
+    const gl = this.gl;
+    gl.useProgram(this.program);
+  }
+
+  addAttribute(name, size, type) {
+    const gl = this.gl;
+    const attributeLocation = gl.getAttribLocation(this.program, name);
+
+    if (attributeLocation >= 0) {
+      // Enable the attribute and set its properties
+      gl.vertexAttribPointer(attributeLocation, size, type, false, 0, 0);
+      gl.enableVertexAttribArray(attributeLocation);
     }
   }
-  
-  export default Program;
-  
+}
+ 
